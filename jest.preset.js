9e2ce1a095df5056ec67cedd436cefe72c78f685
Su Anli <Su@Sus-MacBook-Pro.local>
2021-12-21 16:44:42 +0800

    chore(habit-app): setup test with coverage

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,11 @@
-const nxPreset = require('@nrwl/jest/preset');
+const rnPreset = require('react-native/jest-preset');
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...rnPreset,
+  moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
+  resolver: '@nrwl/jest/plugins/resolver',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native-community|@react-native|@react-navigation)',
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/', '.generated.'],
+};
